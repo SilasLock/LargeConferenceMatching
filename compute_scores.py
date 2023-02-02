0cbdf96e96f707b1cca12a7c62b18bffa644dac8
Jason Hartline <jasonhartline@gmail.com>
2023-02-02 02:19:25 -0600

    specific columnheaders

--- compute_scores.py
+++ compute_scores.py
@@ -10,9 +10,9 @@ def get_scores(config, scores):
     bid2exponent = pd.DataFrame(bid2exponent)
     scores = scores.reset_index().merge(bid2exponent,how='left',on='bid').set_index(['paper','reviewer'])
     epsilon = 0.0000001
-    
+
     #match score: avg of ntpms and nacl
-    scores['ms'] =  (scores['ntpms']  + scores['nacl'])/2 
+    scores['ms'] =  (scores['ntpms']  + scores['nacl'])/2
     #if ntpms is na, then overwrite by nacl
     scores.loc[scores['ntpms'].isna(),'ms'] = scores['nacl'].loc[scores['ntpms'].isna()]
     #if nacl is na, overwrite by ntpms
@@ -24,17 +24,17 @@ def get_scores(config, scores):
     scores.loc[scores['ms'].isna(),'scores_base'] = scores['nk'].loc[scores['ms'].isna()]
     #if keyword score is na, overwrite by match score
     scores.loc[scores['nk'].isna(),'scores_base'] = scores['ms'].loc[scores['nk'].isna()]
-    
+
     # tweaking based on bid value
     condn = scores['is_positive'] & (scores['nk'] < epsilon)
     #if bid is positive and keyword score < epsilon, overwrite by match score
     scores.loc[condn,'scores_base'] = scores['ms'].loc[condn]
-    
+
     #if everything is na, score = 0
     scores.loc[scores['scores_base'].isna(),'scores_base'] = 0.0
     #
     scores['score']  = scores['scores_base']**(1.0/scores['exponent'])
-    
+
     # if score is below this thr, then backoff to keyword score only
     lower_thr = 0.15
     #select all scores less than 0.15
@@ -45,8 +45,8 @@ def get_scores(config, scores):
 
 def compute_scores(config=None):
 
-    scores = pd.read_csv(config['RAW_SCORES_FILE']).set_index(['paper','reviewer'])
-    bids = pd.read_csv(config['BIDS_FILE']).set_index(['paper','reviewer'])
+    scores = pd.read_csv(config['RAW_SCORES_FILE'],use_cols=["paper","reviewer","ntpms","nacl","nk"]).set_index(['paper','reviewer'])
+    bids = pd.read_csv(config['BIDS_FILE'],use_cols=["paper","reviewer","bid"]).set_index(['paper','reviewer'])
     scores = scores.join(bids)
     scores['bid'] = scores['bid'].fillna(config['DEFAULT_BID_WHEN_NO_BIDS'])
 
@@ -59,4 +59,3 @@ def compute_scores(config=None):
     logger.info(f"Caching aggregated score to {config['CACHED_SCORES_FILE']} to save time during next run. To recompute, delete this file and rerun this script.")
     scores.to_csv(config['CACHED_SCORES_FILE'])
     return scores
-
