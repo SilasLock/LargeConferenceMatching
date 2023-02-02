3131065b063956c0bf0558968ef568d4bb088feb
Jason Hartline <jasonhartline@gmail.com>
2023-02-02 01:12:24 -0600

    hotcrp_to_lcm.py: convert emails to numbers, save emails in extra column in each file 'reviewer_email'

--- hotcrp/hotcrp_to_lcm.py
+++ hotcrp/hotcrp_to_lcm.py
@@ -7,7 +7,7 @@ from config import *
 write_csv_orig = write_csv
 def write_csv(x, d):
     write_csv_orig(DATA_WITH_NONREVIEWERS + x, d)
-
+    
 ## update PC preferences.
 ##    - create bids file <= (paper, reviewer, bid)
 ##    - create hotcrp conflicts file (paper, reviewer)
@@ -35,7 +35,8 @@ creates_files = [
 print(f"NEEDS {needs_files}")
 print(f"CREATES {creates_files}")
 print()
-
+print(f"WRITES TO {DATA_WITH_NONREVIEWERS}")
+print()
 
 # read pc data, parse tags as set, only keep pc and ac (remove track-chairs, etc)
 pc_data = [
@@ -48,6 +49,31 @@ pc_data = [
     if AC_TAG in tags or PC_TAG in tags
 ]
 print(f"read PC info [{HOTCRP_PCINFO_CSV}]")
+
+
+# ids start from 1
+id_to_email = dict(enumerate(sorted([pc['email'] for pc in pc_data]),1))
+email_to_id = dict([(e,i) for (i,e) in id_to_email.items()])
+
+
+unknown_reviewer_emails = []
+
+def id_for_email(email):
+    global unknown_reviewer_emails
+    
+    if email in email_to_id:
+        return email_to_id[email]
+
+    i = len(id_to_email) + 1
+    id_to_email[i] = email
+    email_to_id[email] = i
+    unknown_reviewer_emails += [email]
+    
+    return i
+
+def email_for_id(i):
+    return id_to_email(i)
+
 
 ## submitted papers
 paper_data = [r for r in read_json(HOTCRP_DATA_JSON) if r['status'] == "submitted"]
@@ -95,8 +121,9 @@ def get_bid(pid,rid):
 
 bids = [{
     "paper":pid,
-    "reviewer":rid,
-    "bid": get_bid(pid,rid)
+    "reviewer":id_for_email(rid),
+    "bid": get_bid(pid,rid),
+    "reviewer_email":rid
 }
     for pid in paper_ids
     for rid in pc_ids
@@ -153,10 +180,11 @@ def bounded_or_nothing(lookup,pid,rid):
 
 raw_scores = [{
     "paper":pid,
-    "reviewer":rid,
+    "reviewer":id_for_email(rid),
     "ntpms":bounded_or_nothing(ss_score_data,pid,rid),
     "nacl":bounded_or_nothing(acl_score_data,pid,rid),
-    "nk": bounded((20+topic_score_data.get((pid,rid),0)) / 40,0,1)
+    "nk": bounded((20+topic_score_data.get((pid,rid),0)) / 40,0,1),
+    "reviewer_email":rid
 }
     for pid in paper_ids
     for rid in pc_ids
@@ -185,7 +213,8 @@ hotcrp_coi_data = {
 
 conflicts = [{
     'paper':pid,
-    'reviewer':rid
+    'reviewer':id_for_email(rid),
+    'reviewer_email':rid
 } for (pid,rid) in hotcrp_coi_data | ss_coi_data]
 
 
@@ -250,12 +279,13 @@ def get_seniority(pc):
         
     
 props = [{
-    'reviewer':pc['email'],
+    'reviewer':id_for_email(pc['email']),
     'role':"AC" if "ac" in pc['tags'] else "PC",
     "seniority":get_seniority(pc),
     "conflict_papers":str(reviewer_cois.get(pc['email'],[])),
     "region":''.join(filter(lambda x: str.isalnum(x) and str.isascii(x), pc['affiliation'])),
-    "authored":str([])
+    "authored":str([]),
+    "reviewer_email":pc['email']
 }
     for pc in pc_data
 ]
@@ -265,9 +295,28 @@ print(f"wrote reviewer props [{LCM_REVIEWER_PROPS_CSV}]")
 
 print()
 
-write_csv(LCM_COAUTHOR_DISTANCE_CSV, read_csv(COAUTHOR_DISTANCE_FILE_NAME))
-print(f"moved coauthor distance file into correct directory")
+
+try:
+    coauthor_data = [{
+        "reviewer_1":       id_for_email(cd['reviewer_1']),
+        "reviewer_2":       id_for_email(cd['reviewer_2']),
+        "distance":         cd['distance'],
+        "reviewer_1_email": cd['reviewer_1'],
+        "reviewer_2_email": cd['reviewer_2']
+    } for cd in read_csv(COAUTHOR_DISTANCE_FILE_NAME)]
+except:
+    print(f"could not read SS conflict file [{SS_CONFLICTS_CSV}]")
+    coauthor_data = []
+    
+
+if coauthor_data:
+    write_csv(LCM_COAUTHOR_DISTANCE_CSV, coauthor_data)
+    print(f"moved coauthor distance [{COAUTHOR_DISTANCE_FILE_NAME}] to [{LCM_COAUTHOR_DISTANCE_CSV}]")
 
 print()
+
+if unknown_reviewer_emails:
+    print(f"found {len(unknown_reviewer_emails)} emails:")
+    pprint(sorted(unknown_reviewer_emails))
 
 print("done")
